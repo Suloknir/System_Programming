531c83e9cf55018bdc659b5918e6eda002c47fca
Suloknir <jedrzejzawalski@gmail.com>
2026-04-26 19:19:33 +0200

    UPD lab7: finished password hashing program

--- lab7/main.c
+++ lab7/main.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <crypt.h>
+#include <string.h>
 
 int main(const int argc, char *argv[])
 {
     int ret;
     char *salt = NULL;
-    char *to_hash = NULL;
+    char *password = NULL;
     opterr = 0;
-    while ((ret = getopt(argc, argv, "s:h:")) != -1)
+    while ((ret = getopt(argc, argv, "s:p:")) != -1)
     {
         switch (ret)
         {
             case 's':
                 salt = optarg;
                 break;
-            case 'h':
-                to_hash = optarg;
+            case 'p':
+                password = optarg;
                 break;
             case '?':
                 fprintf(stderr, "%s: Option '%c' requires argument\n", argv[0], optopt);
@@ -26,9 +28,23 @@ int main(const int argc, char *argv[])
                 abort();
         }
     }
-    if (salt == NULL || to_hash == NULL)
+    if (salt == NULL || password == NULL)
     {
-        fprintf(stderr, "%s: Parameters 's', 'h' are mandatory\n", argv[0]);
+        fprintf(stderr, "%s: Parameters 's' (salt), 'p' (password) are mandatory\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+    struct crypt_data data;
+    data.initialized = 0;
+    char *hash_fun_id = "$6$";
+    size_t buf_len = strlen(salt) + strlen(hash_fun_id) + 1;
+    char* buffer = malloc(buf_len * sizeof(char));
+    if (buffer == NULL)
+        perror("malloc error");
+    snprintf(buffer, buf_len, "%s%s", hash_fun_id, salt);
+    const char* hash = crypt_r(password, buffer, &data);
+    free(buffer);
+    if (hash == NULL)
+        perror("crypt_r error\n");
+    printf("Hashed password: %s\n", hash);
+    return 0;
 }
