f23972bd8f060878cb4b385b0f2e8f2720c41b9a
Suloknir <jedrzejzawalski@gmail.com>
2026-04-26 18:39:11 +0200

    UPD lab7: Added argument list parsing

--- lab7/main.c
+++ lab7/main.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main(void)
+int main(const int argc, char *argv[])
 {
-    printf("Hello\n");
+    int ret;
+    char *salt = NULL;
+    char *to_hash = NULL;
+    opterr = 0;
+    while ((ret = getopt(argc, argv, "s:h:")) != -1)
+    {
+        switch (ret)
+        {
+            case 's':
+                salt = optarg;
+                break;
+            case 'h':
+                to_hash = optarg;
+                break;
+            case '?':
+                fprintf(stderr, "%s: Option '%c' requires argument\n", argv[0], optopt);
+                exit(EXIT_FAILURE);
+                break;
+            default:
+                abort();
+        }
+    }
+    if (salt == NULL || to_hash == NULL)
+    {
+        fprintf(stderr, "%s: Parameters 's', 'h' are mandatory\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 }
