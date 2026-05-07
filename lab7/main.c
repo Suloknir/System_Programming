dc8de585928ab6bdd9ed55848c785e29664605df
Suloknir <jedrzejzawalski@gmail.com>
2026-05-07 12:38:41 +0200

    UPD lab7: Better data packs distribution, refactored code

--- lab7/main.c
+++ lab7/main.c
@@ -6,21 +6,20 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
+#include <time.h>
 // #include <crypt.h>
-// #include <time.h>
 
-void print_progress(size_t done, size_t to_do, const int max_bars)
-{
-    const float fraction_done = (float) done / to_do;
-    const int bars_to_print = fraction_done * max_bars;
-    printf("\r[");
-    for (int i = 0; i < bars_to_print; i++)
-        printf("=");
-    for (int i = 0; i < max_bars - bars_to_print; i++)
-        printf(" ");
-    printf("]%7.2f%%", fraction_done * 100);
-    fflush(stdout);
-}
+#ifndef CRACK_FOUND
+#define CRACK_FOUND 1
+#endif
+
+#ifndef CRACK_NOT_FOUND
+#define CRACK_NOT_FOUND 0
+#endif
+
+#ifndef CRACK_ERR
+#define CRACK_ERR -1
+#endif
 
 void parse_argv(const int argc, char *const*argv, char **ret_hash, char **ret_filepath, int *ret_n_threads)
 {
@@ -61,64 +60,121 @@ void parse_argv(const int argc, char *const*argv, char **ret_hash, char **ret_fi
     }
 }
 
-void thread_crack(int id, char *pswd, size_t length) {}
-
-bool crack(const char *salted_hash, const char *pswd_path, int n_threads) //todo: refactor that awfulness
+void force_print_progress(size_t done, size_t to_do, int bars)
 {
-    int fd = open(pswd_path, O_RDONLY);
+    const float fraction_done = (float) done / to_do;
+    const int bars_to_print = fraction_done * bars;
+    printf("\r[");
+    for (int i = 0; i < bars_to_print; i++)
+        printf("=");
+    for (int i = 0; i < bars - bars_to_print; i++)
+        printf(" ");
+    printf("] %6.2f%%", fraction_done * 100);
+    fflush(stdout);
+}
+
+void print_progress(size_t done, size_t to_do, int bars, float fps)
+{
+    static bool already_printed = false;
+    static struct timespec last_print;
+    if (!already_printed || done == to_do)
+    {
+        clock_gettime(CLOCK_MONOTONIC, &last_print);
+        force_print_progress(done, to_do, bars);
+        already_printed = true;
+        return;
+    }
+    struct timespec now;
+    clock_gettime(CLOCK_MONOTONIC, &now);
+    const float elapsed = (float) (now.tv_sec - last_print.tv_sec) +
+                          (float) (now.tv_nsec - last_print.tv_nsec) / 1e9;
+    if (elapsed >= 1.0f / fps)
+    {
+        clock_gettime(CLOCK_MONOTONIC, &last_print);
+        force_print_progress(done, to_do, bars);
+    }
+}
+
+void thread_crack(int id, const char *mapped, size_t length) {}
+
+/// If 'CRACK_FOUND' was returned, memory allocated in 'ret found' should be freed manually.
+/// Otherwise, ret_found is not set at all.
+short crack(const char *salted_hash, const char *pswd_path, int n_threads, char **ret_found)
+{
+    const int fd = open(pswd_path, O_RDONLY);
     if (fd == -1)
-        err(EXIT_FAILURE, "open error\n");
+    {
+        fprintf(stderr, "open error\n");
+        return CRACK_ERR;
+    }
     struct stat sb;
     if (fstat(fd, &sb) == -1)
-        err(EXIT_FAILURE, "fstat error\n");
+    {
+        close(fd);
+        fprintf(stderr, "fstat error\n");
+        return CRACK_ERR;
+    }
     size_t file_length = sb.st_size;
-    if (file_length == 0) return false;
-
-    char *pswd = mmap(NULL, file_length, PROT_READ, MAP_PRIVATE, fd, 0);
+    if (file_length == 0)
+        return CRACK_NOT_FOUND;
+    char *mapped = mmap(NULL, file_length, PROT_READ, MAP_PRIVATE, fd, 0);
     close(fd);
-    if (pswd == MAP_FAILED)
-        err(EXIT_FAILURE, "mmap error\n");
-    const char *const start_file = pswd;
-    const char *const end_file = pswd + file_length;
+    if (mapped == MAP_FAILED)
+    {
+        fprintf(stderr, "mmap error\n");
+        return CRACK_ERR;
+    }
     size_t approx_pack_size = file_length / n_threads;
-    if (approx_pack_size == 0) approx_pack_size = 1;
+    if (approx_pack_size == 0)
+        approx_pack_size = 1;
     printf("file_length = %lu\n", file_length);
-    printf("approximate pack size = %lu\n", approx_pack_size);
-    size_t next_start_pack_id = 0;
-    int spawned_threads = 0;
+    printf("approx_pack_size = %lu\n", approx_pack_size);
+    int created_threads = 0;
+    size_t next_start_id = 0;
     for (int i = 0; i < n_threads; i++)
     {
-        size_t start_pack_id = next_start_pack_id;
-        size_t end_pack_id = (i + 1) * approx_pack_size - 1;
-        if (end_pack_id <= start_pack_id) continue;
-        if (start_pack_id >= file_length) break;
-        if (end_pack_id < file_length - 1 && pswd[end_pack_id] != '\n')
+        const size_t pack_start_id = next_start_id;
+        if (pack_start_id >= file_length)
+            break;
+        size_t pack_end_id;
+        if (i == n_threads - 1)
+            pack_end_id = file_length - 1;
+        else
         {
-            char *next_line = memchr(&pswd[end_pack_id], '\n', file_length - end_pack_id);
-            if (next_line != NULL && next_line != end_file - 1)
-                end_pack_id = next_line - start_file;
-            else
+            pack_end_id = (i + 1) * approx_pack_size - 1;
+            if (pack_end_id < pack_start_id)
+                continue;
+            if (pack_end_id < file_length - 1 && mapped[pack_end_id] != '\n')
             {
-                end_pack_id = file_length - 1;
-                i = n_threads - 1; //to stop after just that thread
-                printf("\n-----BREAKING AFTER %d----\n", i);
+                const char *next_line = memchr(&mapped[pack_end_id], '\n', file_length - pack_end_id);
+                if (next_line != NULL)
+                    pack_end_id = next_line - mapped;
+                else
+                {
+                    pack_end_id = file_length - 1;
+                    i = n_threads - 1; //to stop after just that thread
+                }
             }
         }
-        // if (i == n_threads - 1)
-        //     end_pack_id = file_length - 1;
-        printf("--- %d: [start_id: %lu] [end_id: %lu] [end == '\\n' ? %d] [start = '%c']\n", i, start_pack_id, end_pack_id,
-               pswd[end_pack_id] == '\n',
-               pswd[start_pack_id]);
-        spawned_threads++;
-        for (size_t j = start_pack_id; j <= end_pack_id; j++)
+        next_start_id = pack_end_id + 1;
+        printf("--- %d: [start_id: %lu] [end_id: %lu] [pack_size = %lu] [end == '\\n' ? %d] [start = '%c']\n",
+               i,
+               pack_start_id,
+               pack_end_id,
+               pack_end_id - pack_start_id,
+               mapped[pack_end_id] == '\n',
+               mapped[pack_start_id]);
+        for (size_t j = pack_start_id; j <= pack_end_id; j++)
         {
-            printf("%c", pswd[j]);
+            printf("%c", mapped[j]);
+            // print_progress(j - pack_start_id, pack_end_id - pack_start_id, 30, 1.5f);
         }
-        next_start_pack_id = end_pack_id + 1;
+
+        created_threads++;
     }
-    printf("\nSpawned Threads: %d\n", spawned_threads);
-    munmap(pswd, file_length);
-    return false;
+    printf("\nCreated Threads: %d\n", created_threads);
+    munmap(mapped, file_length);
+    return CRACK_NOT_FOUND;
 }
 
 int main(const int argc, char *argv[])
@@ -127,7 +183,7 @@ int main(const int argc, char *argv[])
     char *pswd_path = NULL;
     int n_threads = -1;
     parse_argv(argc, argv, &salted_hash, &pswd_path, &n_threads);
-    // const int max_threads = (int) sysconf(_SC_NPROCESSORS_ONLN);
+    const int max_threads = (int) sysconf(_SC_NPROCESSORS_ONLN);
     if (n_threads == -1)
     {
         //todo: find best nr of threads
@@ -136,8 +192,25 @@ int main(const int argc, char *argv[])
     else
     {
         // if (n_threads > max_threads) n_threads = max_threads;
-        crack(salted_hash, pswd_path, n_threads);
+        char *found_pass = NULL;
+        switch (crack(salted_hash, pswd_path, n_threads, &found_pass))
+        {
+            case CRACK_FOUND:
+                printf("Found password: %s\n", found_pass);
+                free(found_pass);
+                break;
+            case CRACK_NOT_FOUND:
+                printf("Password not found\n");
+                break;
+            case CRACK_ERR:
+                fprintf(stderr, "crack_error\n");
+                exit(EXIT_FAILURE);
+                // ReSharper disable once CppDFAUnreachableCode
+                break;
+            default:
+                fprintf(stderr, "Unexpected crack return\n");
+                abort();
+        }
     }
-
     return 0;
 }
